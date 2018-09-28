5b43b9592c007f0899a44b64175d57ac53f955bb
Fernando Willian <feh.wilinando@gmail.com>
2018-09-27 22:17:10 -0300

    Improving api stock

--- stock/src/main/java/br/com/caelum/application/controllers/StockItemController.java
+++ stock/src/main/java/br/com/caelum/application/controllers/StockItemController.java
@@ -3,13 +3,14 @@ package br.com.caelum.application.controllers;
 import br.com.caelum.domain.models.StockItem;
 import br.com.caelum.domain.repositories.StockRepository;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
+@RequestMapping("stock")
 public class StockItemController {
 
     private final StockRepository repository;
@@ -19,8 +20,9 @@ public class StockItemController {
     }
 
     @GetMapping(value = "{code}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Optional<StockItem> getById(@PathVariable("code") String code){
-        return repository.findByCode(code);
+    public ResponseEntity<StockItem> getById(@PathVariable("code") String code){
+        return repository.findByCode(code).map(ResponseEntity::ok)
+                .orElseGet(ResponseEntity.noContent()::build);
     }
 
 }

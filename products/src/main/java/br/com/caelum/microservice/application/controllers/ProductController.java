6a24f6e92a3e632f88bd5fdf80bf43aa68030324
Fernando Willian <feh.wilinando@gmail.com>
2018-09-27 21:02:53 -0300

    Implemented documentation of api

--- products/src/main/java/br/com/caelum/microservice/application/controllers/ProductController.java
+++ products/src/main/java/br/com/caelum/microservice/application/controllers/ProductController.java
@@ -1,20 +1,23 @@
 package br.com.caelum.microservice.application.controllers;
 
 import br.com.caelum.microservice.domain.models.Product;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+@Api(description = "Api for products", tags = "Products")
 @RestController
-@RequestMapping("products")
+@RequestMapping(value = "products")
 public class ProductController {
 
+    @ApiOperation(value = "Get all products value", notes = "Get all products")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Product> all() {
         return new ArrayList<>();
@@ -23,6 +26,15 @@ public class ProductController {
     @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Product getById(@PathVariable("id") Long id) {
         return new Product("T-Shirt", "The beautiful black t-shirt", BigDecimal.TEN);
+    }
+
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Product> save(@RequestBody Product product){
+
+        URI location = URI.create("/products/2");
+
+        return ResponseEntity.created(location).body(product);
     }
 
 }

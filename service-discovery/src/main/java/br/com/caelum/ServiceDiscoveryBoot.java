985000755f70c5b856234b91e31cf16f19f688cf
Fernando Willian <feh.wilinando@gmail.com>
2018-09-28 01:07:05 -0300

    Implements service discovery server

---
+++ service-discovery/src/main/java/br/com/caelum/ServiceDiscoveryBoot.java
@@ -0,0 +1,13 @@
+package br.com.caelum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDiscoveryBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryBoot.class, args);
+    }
+}

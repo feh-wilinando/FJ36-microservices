90ce0669daf7350302f0fdec1b5c1f16bea71934
Fernando Willian <feh.wilinando@gmail.com>
2018-09-27 21:49:23 -0300

    Added persistence on project

--- stock/src/main/java/br/com/caelum/domain/models/StockItem.java
+++ stock/src/main/java/br/com/caelum/domain/models/StockItem.java
@@ -1,8 +1,15 @@
 package br.com.caelum.domain.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class StockItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String code;
     private BigDecimal quantity;

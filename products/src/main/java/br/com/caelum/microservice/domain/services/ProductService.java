dfd7ee3b7cf6950a27928e7004245b1951060986
Fernando Willian <feh.wilinando@gmail.com>
2018-09-28 21:03:01 -0300

    Implements circuit breaker

--- products/src/main/java/br/com/caelum/microservice/domain/services/ProductService.java
+++ products/src/main/java/br/com/caelum/microservice/domain/services/ProductService.java
@@ -4,9 +4,11 @@ import br.com.caelum.microservice.domain.dtos.ProductDTO;
 import br.com.caelum.microservice.domain.dtos.StockDTO;
 import br.com.caelum.microservice.domain.models.Product;
 import br.com.caelum.microservice.domain.repositories.ProductRepository;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -23,11 +25,13 @@ public class ProductService {
     }
 
 
+    @HystrixCommand(fallbackMethod = "findAllFallback")
     public List<ProductDTO> findAll(){
         return repository.findAll().stream().map(this::mapping)
                 .collect(Collectors.toList());
     }
 
+    @HystrixCommand(fallbackMethod = "findByIdFallback")
     public Optional<ProductDTO> findById(Long id){
         return repository.findById(id).map(this::mapping);
     }
@@ -40,5 +44,22 @@ public class ProductService {
 
         return ProductDTO.buildFrom(product, stock);
 
+    }
+
+
+    public List<ProductDTO> findAllFallback(){
+        return repository.findAll().stream().map(this::mappingFallback)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<ProductDTO> findByIdFallback(Long id){
+        return repository.findById(id).map(this::mappingFallback);
+    }
+
+    private ProductDTO mappingFallback(Product product) {
+        StockDTO stock = new StockDTO();
+        stock.setQuantity(BigDecimal.ZERO);
+
+        return ProductDTO.buildFrom(product, stock);
     }
 }
